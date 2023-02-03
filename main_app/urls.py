96576d7a84fb4f7fae0b4f732a94dce96f3297d1
bardIRL <115611931+bardIRL@users.noreply.github.com>
2023-02-03 15:57:24 -0800

    adds route and view for words index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('words/', views.words_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,17 +2,24 @@ from django.shortcuts import render
 
 words = [
     {
+        'word':'uncouth',
+        'part_of_speech': 'adjective',
+        'definition':'lacking good manners, refinement, or grace, typically in a rude and unpleasant manner',
+        'sentence':'She may embarrass you at the dinner party with her typical, uncouth behavior.',
+    },
+    {
         'word':'onomatopoeia',
         'part_of_speech': 'noun',
         'definition':'the formation of a word from a sound associated with what is named',
-        'sentence':'The cat breed "Mao" is an obvious example of onomatopoeia, since cats go "Mao".'
+        'sentence':'The cat breed "Mao" is an obvious example of onomatopoeia, since cats go "Mao".',
     },
     {
         'word':'sanctimonious',
         'part_of_speech': 'adjective',
         'definition':'making a show of being morally superior to other people',
-        'sentence':'I could not stand his sanctimonious speech about the importance of sobriety when he gets plastered every other weekend.'
-    }
+        'sentence':'I could not stand his sanctimonious speech about the importance of sobriety when he gets plastered every other weekend.',
+    },
+
 ]
 
 # Create your views here.
@@ -22,3 +29,8 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def words_index(request):
+    return render(request, 'words/index.html', {
+        'words': words
+    })
