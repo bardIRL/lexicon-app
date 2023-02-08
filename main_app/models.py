ec2ba0d7bc01f784f507de4ff623d6901b70d3b2
bardIRL <115611931+bardIRL@users.noreply.github.com>
2023-02-07 17:45:02 -0800

    creates Word model, adds styling to cards on index page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    word = models.CharField(max_length=100)
+    definition = models.TextField(max_length=250)
+    sentence = models.TextField(max_length=250)
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,37 +1,6 @@
 from django.shortcuts import render
 
-words = [
-    {
-        'word':'uncouth',
-        'part_of_speech': 'adjective',
-        'definition':'lacking good manners, refinement, or grace, typically in a rude and unpleasant manner',
-        'synonyms': 'uncivilized, uncultured, plebeian, rough, unsophisticated',
-        'sentence':'She may embarrass you at the dinner party with her typical, uncouth behavior.',
-    },
-    {
-        'word':'peregrinate',
-        'part_of_speech': 'verb',
-        'definition':'to travel or wander around from place to place, especially on foot',
-        'synonyms': 'hike, journey, travel, walk, rove,perambulate',
-        'sentence':'We peregrinated over Stanmore, and visited the Castles of Bowes and Brougham.',
-    },
-    {
-        'word':'sanctimonious',
-        'part_of_speech': 'adjective',
-        'definition':'making a show of being morally superior to other people',
-        'synonyms': 'self-righteous, holier-than-thou, churchy, pious, hypocritical, unctuous',
-        'sentence':'I could not stand his sanctimonious speech about the importance of sobriety when he gets plastered every other weekend.',
-    },
-     {
-        'word':'obsequious',
-        'part_of_speech': 'adjective',
-        'definition':'obedient or attentive to an excessive or servile degree',
-        'synonyms': 'unctuous, fawning, servile, submissive',
-        'sentence':'Barrow was positively obsequious to me until he learned that I too was the son of a laboring man.',
-    },
-
-
-]
+from .models import Word
 
 # Create your views here.
 
@@ -42,6 +11,7 @@ def about(request):
     return render(request, 'about.html')
 
 def words_index(request):
+    words = Word.objects.all()
     return render(request, 'words/index.html', {
         'words': words
     })
